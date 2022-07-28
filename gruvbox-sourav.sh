cc3f6e8fea41b7d9bcb0bc015b62fc507859ae38
Sourav <souravgope765@gmail.com>
2022-07-29 00:34:48 +0530

    script: script for installation of  my gruvbox dotfiles

---
+++ gruvbox-sourav.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo -e "starting the installation process"
+sleep 1s
+
+echo -e "installing required packages using pacman"
+sleep 1s
+sudo pacman -S git jq clash feh yay dunst i3-gaps automake make autoconf dbus-python i3blocks mpd ncmpcpp neovim rofi i3lock gnome-screenshot alacritty ttf-font-awesome playerctl
+sleep 1s
+echo -e "installing packages from aur using yay"
+yay -S polybar cava picom-jonaburg-git
+
+echo -e "installation of packages done"
+sleep 1s 
+echo -e "fetching the gruvbox dotfiles from github"
+sleep 1s
+
+git clone --bare https://github.com/sourav2k/dotfiles.git -b i3-gaps-gruvbox $HOME/.cfg
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
+
