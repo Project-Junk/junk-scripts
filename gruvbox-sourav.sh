45483c5a8b36b71ee3bb967ff7cab12b3670e898
Sourav <souravgope765@gmail.com>
2022-11-23 23:26:29 +0530

    script: add conky-lua as dependency

--- gruvbox-sourav.sh
+++ gruvbox-sourav.sh
@@ -5,17 +5,23 @@ sleep 1s
 
 echo -e "installing required packages using pacman"
 sleep 1s
-sudo pacman -S git jq clash feh yay dunst i3-gaps automake make autoconf dbus-python i3blocks mpd ncmpcpp neovim rofi i3lock gnome-screenshot alacritty ttf-font-awesome playerctl
+
+sudo pacman -S git jq clash feh dunst i3-gaps automake make autoconf dbus-python i3blocks mpd ncmpcpp neovim rofi i3lock gnome-screenshot alacritty ttf-font-awesome playerctl
 sleep 1s
+
+git clone https://aur.archlinux.org/yay.git ~/yay 
+cd ~/yay && makepkg -si
+sleep 1s
+
 echo -e "installing packages from aur using yay"
-yay -S polybar cava picom-jonaburg-git
+yay -S polybar cava picom-jonaburg-git conky-lua
 
 echo -e "installation of packages done"
 sleep 1s 
 echo -e "fetching the gruvbox dotfiles from github"
 sleep 1s
 
-git clone --bare https://github.com/sourav2k/dotfiles.git -b i3-gaps-gruvbox $HOME/.cfg
+git clone --bare https://github.com/sourav2k/dotfiles.git -b i3-gaps-thefallnn $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
