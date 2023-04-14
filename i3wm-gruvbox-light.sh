89a67cf7b736ffb1bfaa8d45ec7cbf7e9e8adba6
sourav <thefallnn@gmail.com>
2023-04-14 12:18:46 +0530

    add:gruvbox light theme i3wm installation script

---
+++ i3wm-gruvbox-light.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo -e "starting the installation process"
+sleep 1s
+
+echo -e "installing required packages using pacman"
+sleep 1s
+
+sudo pacman -S git zathura zathura-pdf-mupdf ranger jq glava clash feh dunst i3-gaps automake make autoconf dbus-python i3blocks mpd ncmpcpp neovim rofi i3lock gnome-screenshot alacritty ttf-font-awesome playerctl xwallpaper
+sleep 1s
+
+git clone https://aur.archlinux.org/yay.git ~/yay 
+cd ~/yay && makepkg -si
+sleep 1s
+
+echo -e "installing packages from aur using yay"
+yay -S polybar cava picom-jonaburg-git conky-lua
+
+echo -e "installation of packages done"
+sleep 1s 
+echo -e "fetching the gruvbox dotfiles from github"
+sleep 1s
+
+git clone --bare https://github.com/thefallnn/dotfiles.git -b i3wm-gruvbox-light $HOME/.cfg
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
+
